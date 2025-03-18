518af2bf6f621b9f3cb1cb997d44b4123881e7f6
Augustin Gall <augustin.gall@edu.devinci.fr>
2025-03-18 16:50:04 +0100

    Ajout du fichier colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF3f8df3);
+  static const Color secondary = Color(0xFF000812);
+  static const Color whiteText = Color(0xFFECECF4);
+  static const Color blackText = Color(0xFF000000);
+  static const Color title = Color(0xFFFFFFFF);
+  static const Color background = Color(0xFF0E0F1A);
+}
